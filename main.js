e1e40e6e658b374c2423e24361aeb57289a7de48
krzyssota <krzys.sota@gmail.com>
2020-04-27 22:38:00 +0200

    sth is working already

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
 ;
 ;
-var jsonQuiz_1 = "{\n    \"introduction\":\"Welcome to my quiz\",\n    \"questions\":{\n        \"1\": [\"2+3\", \"5\", 4],\n        \"2\": [\"2-(-24:4)\", \"8\", 10],\n        \"3\": [\"2*5\", \"10\", 10],\n        \"4\": [\"3:1\", \"3\", 10]\n    }\n}";
-var quiz_1 = JSON.parse(jsonQuiz_1);
-console.log(quiz_1.introduction);
-console.log(quiz_1.questions[2][0]);
-var card_numer = 1;
-var ans_el = document.getElementById("players_anwser");
-ans_el.addEventListener('input', function (ev) {
-    if (ans_el.value === quiz_1.questions[card_numer][1]) {
-        ans_el.style.backgroundColor = "rgb(55, 196, 55)";
+var jsonQuiz_1 = "{\n    \"introduction\": \"Welcome to my quiz\",\n    \"questions\":{\n        \"1\": [\"10+2\", \"12\", 4],\n        \"2\": [\"2-(-24:4)\", \"8\", 10],\n        \"3\": [\"2*5\", \"10\", 10],\n        \"4\": [\"3:1\", \"3\", 10]\n    },\n    \"size\": \"4\"\n}";
+var curr_quiz = JSON.parse(jsonQuiz_1);
+var introduction_el = document.getElementById("introduction");
+var your_anwser_label = document.getElementById("your_anwser_label");
+var question_el = document.getElementById("question");
+introduction_el.innerHTML = "Introduction: " + "<br>" + curr_quiz.introduction;
+var card_number = 0;
+var players_anwser_el = document.getElementById("players_anwser");
+players_anwser_el.addEventListener('input', function (ev) {
+    if (players_anwser_el.value === curr_quiz.questions[card_number][1]) {
+        players_anwser_el.style.backgroundColor = "rgb(55, 196, 55)";
     }
     else {
-        ans_el.style.backgroundColor = "white";
+        players_anwser_el.style.backgroundColor = "red";
     }
+});
+var button_prev_el = document.getElementById("prev_button");
+button_prev_el.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    if (card_number > 0) {
+        card_number--;
+    }
+    var question_el = document.getElementById("question");
+    if (card_number == 0) {
+        // hide question number
+        introduction_el.style.visibility = "visible";
+        question_el.style.visibility = "hidden";
+        players_anwser_el.style.visibility = "hidden";
+    }
+    else {
+        question_el.style.visibility = "visible";
+        question_el.innerHTML = curr_quiz.questions[card_number][0] + " = ";
+        // reset input text value
+        document.querySelector(".quiz_card > form:nth-child(1)").reset();
+        players_anwser_el.style.backgroundColor = "white";
+    }
+});
+var button_next_el = document.getElementById("next_button");
+button_next_el.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    if (card_number < curr_quiz.size) {
+        card_number++;
+    }
+    if (card_number == 1) {
+        // start the clock
+        // display question number
+        introduction_el.style.visibility = "hidden";
+        players_anwser_el.style.visibility = "visible";
+        question_el.style.visibility = "visible";
+    }
+    question_el.innerHTML = curr_quiz.questions[card_number][0] + " = ";
+    // reset input text value
+    document.querySelector(".quiz_card > form:nth-child(1)").reset();
+    players_anwser_el.style.backgroundColor = "white";
 });
--- main.ts
+++ main.ts
@@ -5,28 +5,80 @@ interface IQuestions {
 interface IQuiz {
     introduction: string;
     questions: IQuestions;
+    size: number;
 };
 let jsonQuiz_1: string = `{
-    "introduction":"Welcome to my quiz",
+    "introduction": "Welcome to my quiz",
     "questions":{
-        "1": ["2+3", "5", 4],
+        "1": ["10+2", "12", 4],
         "2": ["2-(-24:4)", "8", 10],
         "3": ["2*5", "10", 10],
         "4": ["3:1", "3", 10]
-    }
+    },
+    "size": "4"
 }`;
 
-let quiz_1: IQuiz = JSON.parse(jsonQuiz_1);
-console.log(quiz_1.introduction)
-console.log(quiz_1.questions[2][0])
+let curr_quiz: IQuiz = JSON.parse(jsonQuiz_1);
 
-let card_numer = 1;
+let introduction_el = (<HTMLInputElement>document.getElementById("introduction"));
+let your_anwser_label = (<HTMLInputElement>document.getElementById("your_anwser_label"));
+let question_el = (<HTMLInputElement>document.getElementById("question"));
 
-let ans_el = (<HTMLInputElement>document.getElementById("players_anwser"))
-ans_el.addEventListener('input', (ev: InputEvent) => {
-    if(ans_el.value === quiz_1.questions[card_numer][1]) {
-        ans_el.style.backgroundColor = "rgb(55, 196, 55)"
+introduction_el.innerHTML = "Introduction: " + "<br>" + curr_quiz.introduction;
+
+let card_number = 0;
+
+let players_anwser_el = (<HTMLInputElement>document.getElementById("players_anwser"))
+players_anwser_el.addEventListener('input', (ev: InputEvent) => { // TODO zmienić to na przycisk submit anwser ktory koloruje pole
+    if(players_anwser_el.value === curr_quiz.questions[card_number][1]) {
+        players_anwser_el.style.backgroundColor = "rgb(55, 196, 55)"
     } else {
-        ans_el.style.backgroundColor = "white";
+        players_anwser_el.style.backgroundColor = "red";
     }
+})
+
+let button_prev_el = (<HTMLInputElement>document.getElementById("prev_button"))
+button_prev_el.addEventListener('click', (ev: MouseEvent) => {
+    ev.preventDefault();
+
+    if(card_number > 0) {
+        card_number--;
+    }
+    let question_el = (<HTMLInputElement>document.getElementById("question"));
+    if(card_number == 0) {
+        // hide question number
+        introduction_el.style.visibility = "visible";
+
+        question_el.style.visibility = "hidden";
+        players_anwser_el.style.visibility = "hidden";
+    } else {
+        question_el.style.visibility = "visible";
+        question_el.innerHTML = curr_quiz.questions[card_number][0] + " = ";
+
+        // reset input text value
+        (<HTMLFormElement>document.querySelector(".quiz_card > form:nth-child(1)")).reset();
+        players_anwser_el.style.backgroundColor = "white";
+    }
+})
+
+let button_next_el = (<HTMLInputElement>document.getElementById("next_button"))
+button_next_el.addEventListener('click', (ev: MouseEvent) => {
+    ev.preventDefault();
+
+    if(card_number < curr_quiz.size) {
+        card_number++;
+    }
+    if(card_number == 1) {
+        // start the clock
+        // display question number
+    
+        introduction_el.style.visibility = "hidden";
+        players_anwser_el.style.visibility = "visible";
+        question_el.style.visibility = "visible";
+    }
+    question_el.innerHTML = curr_quiz.questions[card_number][0] + " = ";
+    // reset input text value
+    (<HTMLFormElement>document.querySelector(".quiz_card > form:nth-child(1)")).reset();
+    players_anwser_el.style.backgroundColor = "white";
+
 })
